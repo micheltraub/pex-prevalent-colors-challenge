36a97ba2b4efb65fac652690951d18ff37323d94
Michel <michel@swipads.com>
2022-11-04 22:58:55 -0400

    more unit tests

---
+++ internal/app/csvtohtml/csvtohtml_test.go
@@ -0,0 +1,3 @@
+package csvtohtml_test
+
+//todo: test
--- pkg/html/html_test.go
+++ pkg/html/html_test.go
@@ -1,3 +1,35 @@
 package html_test
 
-//TODO: implement testing
+import (
+	"errors"
+	"os"
+	"pex-prevalent-colors-challenge/pkg/html"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type htmlTestSuite struct {
+	suite.Suite
+}
+
+func (s *htmlTestSuite) SetupTest() {
+
+}
+
+func TestHtmlTestSuite(t *testing.T) {
+	suite.Run(t, &htmlTestSuite{})
+}
+
+func (s *htmlTestSuite) TestCsv_OpenCsvOnBrowser() {
+	err := html.CreateHtmlFromCsv("test/test.csv", "test/result.tmpl", "test/index.html")
+
+	if err != nil {
+		s.Fail(err.Error())
+	}
+
+	//read the file to check if data was written
+	if _, err := os.Stat("test/index.html"); errors.Is(err, os.ErrNotExist) {
+		s.Fail(err.Error())
+	}
+}
